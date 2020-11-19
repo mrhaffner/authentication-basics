daf6594519b9a0db007481b6dce58ffe0af8791d
Matt Haffner <mrhaffner13@gmail.com>
2020-11-19 13:19:26 -0600

    added bcryptjs support

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
 
 const mongoDb = "mongodb+srv://matt:higuy@cluster0.tlaah.mongodb.net/basics?retryWrites=true&w=majority";
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
@@ -32,10 +33,15 @@ passport.use(
         if (!user) {
           return done(null, false, { msg: "Incorrect username" });
         }
-        if (user.password !== password) {
-          return done(null, false, { msg: "Incorrect password" });
-        }
-        return done(null, user);
+        bcrypt.compare(password, user.password, (err, res) => {
+            if (res) {
+              // passwords match! log user in
+              return done(null, user)
+            } else {
+              // passwords do not match!
+              return done(null, false, {msg: "Incorrect password"})
+            }
+        })
       });
     })
   );
@@ -68,15 +74,20 @@ app.get("/log-out", (req, res) => {
 });
 
 app.post("/sign-up", (req, res, next) => {
-    const user = new User({
-      username: req.body.username,
-      password: req.body.password
-    }).save(err => {
-      if (err) { 
-        return next(err);
-      };
-      res.redirect("/");
-    });
+    bcrypt.hash(req.body.password, 10, (err, hashed) => {
+        if (err) {
+            return next(err)
+        }
+        const user = new User({
+            username: req.body.username,
+            password: hashed
+        }).save(err => {
+            if (err) { 
+              return next(err);
+            };
+            res.redirect("/");
+          });
+    })
 });
 
 app.post(
