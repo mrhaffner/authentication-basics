2fc5f0ac208e30b994dde9acc16ca0630ed101dd
Matt Haffner <mrhaffner13@gmail.com>
2020-11-20 23:50:38 -0600

    added express session secret to .env

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ passport.deserializeUser(function(id, done) {
     });
 });
 
-app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(session({ secret: process.env.SECRET, resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
